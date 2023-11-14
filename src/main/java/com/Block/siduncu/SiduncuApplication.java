2059bb0960d506bc3b61ecaec53fea900545c777
lukaku <lucascarballo8819@gmail.com>
2023-11-14 00:12:59 -0300

    primer commit

---
+++ src/main/java/com/Block/siduncu/SiduncuApplication.java
@@ -0,0 +1,13 @@
+package com.Block.siduncu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiduncuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiduncuApplication.class, args);
+	}
+
+}
